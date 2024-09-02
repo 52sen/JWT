3c75a7b5710ac25caebf14ce39b218244a1ed9c9
52sen <sein.jung@g2e.co.kr>
2024-09-03 05:44:51 +0900

    추가: :tada: 로그인 로직 구현

--- src/main/java/com/example/jwt/config/SecurityConfig.java
+++ src/main/java/com/example/jwt/config/SecurityConfig.java
@@ -1,18 +1,31 @@
 package com.example.jwt.config;
 
+import com.example.jwt.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 // 이 클래스가 Configuration에 관리되기 위해
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+
+    // AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -20,6 +33,12 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    //AuthenticationManager Bean 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
 
     //CROS,로그인방식 등 filter => 메서드들을 Bean으로 설정
    @Bean
@@ -40,6 +59,11 @@ public class SecurityConfig {
                        .requestMatchers("/login", "/", "/join").permitAll()
                        .requestMatchers("/admin").hasRole("ADMIN")
                        .anyRequest().authenticated()); // 다른 접근은 로그인한 사용자만 사용할 수 있도록
+
+       // 필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함)
+       // 따라서 등록 필요
+       http
+               .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
 
        //세션 설정
        http
--- src/main/java/com/example/jwt/repository/UserRepository.java
+++ src/main/java/com/example/jwt/repository/UserRepository.java
@@ -6,4 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
     Boolean existsByUsername(String username);
+
+
+    //username을 받아 DB에서 회원 조회
+    UserEntity findByUsername(String username);
 }
